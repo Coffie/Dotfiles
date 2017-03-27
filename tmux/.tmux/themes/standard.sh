cd21fd3e950c88f76870882e7d0b8ef895332083
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2017-03-27 14:54:30 +0200

    Fix tmux folder structure

--- tmux/.tmux/themes/standard.sh
+++ tmux/.tmux/themes/standard.sh
@@ -70,12 +70,12 @@ set -g status-bg colour234
 set -g status-fg colour137
 set -g status-attr dim
 set -g status-left ''
-set -g status-right '#[fg=colour233,bg=colour241,bold] %d/%m #[fg=colour233,bg=colour245,bold] %H:%M:%S '
+set -g status-right '#[fg=colour233,bg=colour241,bold] %d/%m #[fg=colour233,bg=colour245,bold] %H:%M '
 set -g status-right-length 50
 set -g status-left-length 20
 
 setw -g window-status-current-fg colour81
-setw -g window-status-current-bg colour238
+setw -g window-status-current-bg colour109
 setw -g window-status-current-attr bold
 setw -g window-status-current-format ' #I#[fg=colour250]:#[fg=colour255]#W#[fg=colour50]#F '
 

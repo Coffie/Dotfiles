d46cffaefa1af6e0bef2a220883f60f79d685ef8
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2017-11-15 09:46:11 +0100

    Add zsh plugin

--- install/zsh.sh
+++ install/zsh.sh
@@ -5,6 +5,8 @@ THEME="https://github.com/bhilburn/powerlevel9k.git"
 DEST="/custom/themes/powerlevel9k"
 ZSHHIGH="https://github.com/zsh-users/zsh-syntax-highlighting.git" 
 ZSHHIGHDEST="$HOME/.dotfiles/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+ZSHAUTO="https://github.com/zsh-users/zsh-autosuggestions"
+ZSHAUTODEST="$HOME/.dotfiles/zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
 if [ ! -d $DIRECTORY ]; then
 	echo -e "\n\nOh-my-zsh not installed"
@@ -32,6 +34,15 @@ then
 	echo -e "\n\nInstalling custom plugins"
 	echo -e  "===============================\n\n"
 	git clone $ZSHHIGH $ZSHHIGHDEST
+else
+	echo -e "\n\nAll plugins installed"
+fi
+
+if [ ! -d $ZSHAUTODEST ];
+then
+	echo -e "\n\nInstalling custom plugins"
+	echo -e  "===============================\n\n"
+	git clone $ZSHAUTO $ZSHAUTODEST
 else
 	echo -e "\n\nAll plugins installed"
 fi
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -52,7 +52,7 @@ HIST_STAMPS="yyyy-mm-dd"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git pip python brew osx wd tmux debian react-native zsh-syntax-highlighting)
+plugins=(git pip python brew osx wd tmux debian react-native zsh-autosuggestions zsh-syntax-highlighting)
 
 # User configuration
 

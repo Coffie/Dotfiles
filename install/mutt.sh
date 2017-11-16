8740c9bcb1a54fcc650fe43a9bea3cbc0b25a52c
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2017-11-16 16:14:26 +0100

    Fix mutt.sh

--- install/mutt.sh
+++ install/mutt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ALIAS="$HOME/.dotfiles/mutt/.mutt/aliases/.alias_include"
-MUTTRC="$HOME/.dotfiles/mutt/.muttrc.local"
+MUTTRC="$HOME/.dotfiles/mutt/.mutt/local/.muttrc.local"
 
 echo -e "\n\nConfiguring local settings"
-echo -e  "===============================\n\n"
+echo -e  "===============================\n"
 if [ ! -f $ALIAS ]; then
 	echo -e "Creating main alias file, edit $ALIAS to include desired aliases\n"
 	cp ~/.dotfiles/mutt/.mutt/aliases/alias_template $ALIAS
@@ -13,7 +13,7 @@ fi
 
 if [ ! -f $MUTTRC ]; then
 	echo -e "Creating local muttrc file, edit $MUTTRC to local settings\n"
-	cp ~/.dotfiles/mutt/muttrc.local.template $MUTTRC
+	cp ~/.dotfiles/mutt/.mutt/local/muttrc.local.template $MUTTRC
 else
 	echo -e "Local muttrc already exists... Skipping\n"
 fi

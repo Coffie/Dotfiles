7b5f07a3e8b45c836de28cb381ff293176214903
Nysæther, Christoffer B <Christoffer.B.Nysather@dnb.no>
2020-11-12 17:57:45 +0100

    add hammerspoon

---
+++ mac/hammerspoon/init.lua
@@ -0,0 +1,43 @@
+hyper = {"ctrl", "alt", "cmd", "shift"}
+
+-- Generic Helpers
+
+function printt(table)
+    for k, v in pairs(table) do
+        print(k .. "--\t" .. tostring(v))
+    end
+end
+
+hs.urlevent.bind("alert", function(eventName, params)
+    hs.alert.show(params['msg'])
+end)
+
+
+function clickMenuItem(app, item)
+    app = hs.application.find(app)
+    app:selectMenuItem(item)
+end
+
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+function require_if_exists(file)
+    if file_exists(file .. ".lua") then
+        print(" #### Loading " .. file)
+        require(file)
+    end
+end
+
+require_if_exists("autoreload")
+-- hs.loadSpoon("ReloadConfiguration")
+-- spoon.ReloadConfiguration:start()
+
+require_if_exists("keyboard")
+-- require_if_exists("yabai")
+require_if_exists("auto-audio")
+-- require_if_exists("cpm-backup")
+-- require_if_exists("cpm-backup2")
+-- require_if_exists("zoom-mute")
+require_if_exists("multidisplay-black")
---
+++ mac/hammerspoon/keyboard.lua
@@ -0,0 +1,51 @@
+ -- Convenient Keyboard shortcut actions
+ 
+ function openTerminal()
+     -- iterm
+     app = hs.application.find("iTerm2")
+     if app == nil then
+         hs.application.launchOrFocus("iTerm2")
+     else
+         app:selectMenuItem("New Window")
+     end
+ end
+
+ function openBrowser()
+     app = hs.application.find("Google Chrome")
+     if app == nil then
+         hs.application.launchOrFocus("Google Chrome")
+     end
+     app:selectMenuItem("New Window")
+ end
+
+ -- ⌘ + ⏎ Opens New Terminal
+hs.hotkey.bind({"cmd", "alt"}, "return", openTerminal)
+
+-- ⌘ + ⇧ + ⏎ Opens New Browser Window
+hs.hotkey.bind({"cmd","shift"}, "return", openBrowser
+-- Hyper+V types contents of clipboard
+hs.hotkey.bind(hyper, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
+-- Hyper+` Brings up Hammerspoon console
+hs.hotkey.bind(hyper, "`", function() hs.openConsole() end)
+
+-- Launch or Focus Activity Monitor
+hs.hotkey.bind(hyper, "M", function() hs.application.launchOrFocus("Activity Monitor") end)
+
+-- Launch or Focus Activity Monitor
+hs.hotkey.bind(hyper, "S", function() hs.application.launchOrFocus("Spotify") end)
+
+-- Provides a keyboard based window switcher (instead of app switcher)
+hs.hotkey.bind({"cmd", "alt"}, "tab", function() hs.hints.windowHints() end)
+
+-- Hyper+F makes toggles app zoom
+hs.hotkey.bind(hyper, "F", function() hs.window.focusedWindow():toggleZoom() end)
+
+-- Ctrl+Cmd + Escape -- Sleeps the Computer
+hs.hotkey.bind({"ctrl", "cmd"}, "escape", function() hs.caffeinate.systemSleep() end)
+
+-- Ctrl+Shift + Escape -- Sleeps the displays
+hs.hotkey.bind({"ctrl", "shift"}, "escape", function() os.execute("pmset displaysleepnow") end)
+
+-- Ctrl+Cmd+Alt + P -- Toggle Caps Lock
+hs.hotkey.bind({"ctrl", "cmd", "alt"}, "P", function() hs.hid.capslock.toggle() end)

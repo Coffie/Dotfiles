0d8ac16af866e22c5d4ab77041966dcf455fd61b
Nysæther, Christoffer B <Christoffer.B.Nysather@dnb.no>
2021-01-14 09:38:14 +0100

    zinit handles prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,4 @@
 # To customize prompt, run `p10k configure` or edit $HOME/p10k.zsh.
-source "$HOME/.p10k.zsh"
+# zinit handles loading of p10k
+# source "$HOME/.p10k.zsh"
 # [[ -f " $HOME/.p10k.zsh" ]] && source "$HOME/.p10k.zsh"

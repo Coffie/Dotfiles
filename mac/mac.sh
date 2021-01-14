25b8d87479c614ea84e0bf1385927b7a874db597
Nysæther, Christoffer B <Christoffer.B.Nysather@dnb.no>
2021-01-14 09:36:12 +0100

    inital mac settings

--- mac/mac.sh
+++ mac/mac.sh
@@ -14,4 +14,34 @@ sudo nvram SystemAudioVolume=" "
 # Increase sound quality for Bluetooth headphones/headsets
 defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 
+# Dock
+# Set the icon size of Dock items 
+defaults write com.apple.dock tilesize -int 28
+
+# Set the icon size of magnification of dock
+# defaults write com.apple.dock largesize -int 64
+
+
+# Show only open applications in the Dock
+defaults write com.apple.dock static-only -bool true
+
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
 

8b7df9593eb41aa1a4c5973e24d82a4738a71714
Christoffer B. Nysæther <christoffer.b.nysather@dnb.no>
2023-02-02 12:52:23 +0100

    use double quotes

--- config/nvim/lua/coffie/packer.lua
+++ config/nvim/lua/coffie/packer.lua
@@ -12,41 +12,43 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-tree/nvim-tree.lua'
-  use 'nvim-tree/nvim-web-devicons'
-  use 'christoomey/vim-tmux-navigator'
-  use 'nvim-lualine/lualine.nvim'
-  use 'ellisonleao/gruvbox.nvim'
-  use 'mbbill/undotree'
-  use 'tpope/vim-fugitive'
-  use 'airblade/vim-gitgutter'
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use "wbthomason/packer.nvim"
+  use "nvim-tree/nvim-tree.lua"
+  use "nvim-tree/nvim-web-devicons"
+  use "christoomey/vim-tmux-navigator"
+  use "nvim-lualine/lualine.nvim"
+  use "ellisonleao/gruvbox.nvim"
+  use "mbbill/undotree"
+  use "tpope/vim-fugitive"
+  use "tpope/vim-commentary"
+  use "tpope/vim-surround"
+  use "airblade/vim-gitgutter"
+  use("nvim-treesitter/nvim-treesitter", {run = ':TSUpdate'})
   use {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { {"nvim-lua/plenary.nvim"} }
   }
 
   use {
-    'VonHeikemen/lsp-zero.nvim',
+    "VonHeikemen/lsp-zero.nvim",
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      {"neovim/nvim-lspconfig"},
+      {"williamboman/mason.nvim"},
+      {"williamboman/mason-lspconfig.nvim"},
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+      {"hrsh7th/nvim-cmp"},
+      {"hrsh7th/cmp-buffer"},
+      {"hrsh7th/cmp-path"},
+      {"saadparwaiz1/cmp_luasnip"},
+      {"hrsh7th/cmp-nvim-lsp"},
+      {"hrsh7th/cmp-nvim-lua"},
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      {"L3MON4D3/LuaSnip"},
+      {"rafamadriz/friendly-snippets"},
     }
   }
 

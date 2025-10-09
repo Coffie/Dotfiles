e44838ccf033c787a5fc20e643844ffd8a989951
Christoffer Nysæther <github@nysaether.no>
2025-10-09 13:47:38 +0200

    add catpuccin temporary for colorscheme

--- config/nvim/lua/coffie/plugins/colorscheme.lua
+++ config/nvim/lua/coffie/plugins/colorscheme.lua
@@ -21,3 +21,22 @@ return {
     vim.cmd.colorscheme("everblush")
   end,
 }
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1500,
+--   config = function()
+--     require("catppuccin").setup({
+--       flavour = "mocha",
+--       integrations = {
+--         cmp = true,
+--         -- gitsigns = true,
+--         nvimtree = true,
+--         treesitter = true,
+--       },
+--       no_italics = true,
+--       no_underline = true,
+--     })
+--     -- vim.cmd.colorscheme("catppuccin")
+--   end,
+-- }
